55268e600ff3bf1663a3bfad1e3ee58723c29e2d
Gr4vity4 <prawared.bw@gmail.com>
2019-08-22 12:09:41 +0700

    add blocks

---
+++ blocks/blocks_light-cup.js
@@ -0,0 +1,18 @@
+Blockly.Blocks['light_cup_block'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("LIGHT CUP");
+    this.appendValueInput("LIGHT_CUP_LED_PIN")
+        .setCheck("Number")
+        .appendField("LED PIN");
+    this.appendValueInput("LIGHT_CUP_SWITCH_PIN")
+        .setCheck("Number")
+        .appendField("SWITCH PIN");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
---
+++ blocks/generators_light-cup.js
@@ -0,0 +1,7 @@
+Blockly.JavaScript['light_cup_block'] = function(block) {
+  var value_light_cup_led_pin = Blockly.JavaScript.valueToCode(block, 'LIGHT_CUP_LED_PIN', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_light_cup_switch_pin = Blockly.JavaScript.valueToCode(block, 'LIGHT_CUP_SWITCH_PIN', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
